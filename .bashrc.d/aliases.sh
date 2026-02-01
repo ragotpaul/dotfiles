271e41fba6e463dd81fcdcc883e305f2091e220e
Paul Ragot <ragot.paul12@gmail.com>
2026-02-01 01:55:03 +0100

    feat: add aliases for common shell commands

---
+++ .bashrc.d/aliases.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# General aliases
+alias ls='ls --color=auto'
+alias ll='ls -lh --color=auto'
+alias la='ls -lah --color=auto'
+alias grep='grep --color=auto'
+
+# Git aliases
+alias gll='git log --oneline --graph --decorate --all'
+alias gst='git status -unormal --ignored'
